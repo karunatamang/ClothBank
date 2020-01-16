c04eceb4f0096bbd19fc47dc635f331076d31f32
karunatamang <korunatamang12@gmail.com>
2020-01-15 20:14:42 -0800

    Api

---
+++ app/src/main/java/com/example/clothbank/api/UserApi.java
@@ -0,0 +1,31 @@
+package com.example.clothbank.api;
+
+import com.example.clothbank.model.Donation;
+import com.example.clothbank.model.User;
+import com.example.clothbank.response.LoginResponse;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+
+public interface UserApi {
+    @POST("login")
+    Call<LoginResponse> userlogin(@Body User user);
+
+    @GET("user")
+    Call<List<User>>  showUser();
+
+    @POST("register")
+    Call<Void> addUser(@Body User user);
+
+    @PUT("update/{id}")
+    Call<String> updateUser(@Path("id") String id, @Body User user);
+
+
+
+}
