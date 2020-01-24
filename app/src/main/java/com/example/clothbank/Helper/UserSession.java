1d3b3bbae221533eb0684e5616839071d40cb3b8
karunatamang <korunatamang12@gmail.com>
2020-01-23 19:44:14 -0800

    usersession is made inside helper package

--- app/src/main/java/com/example/clothbank/Helper/UserSession.java
+++ app/src/main/java/com/example/clothbank/Helper/UserSession.java
@@ -1,4 +1,59 @@
 package com.example.clothbank.Helper;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.clothbank.model.User;
+import com.google.gson.Gson;
+
 public class UserSession {
+
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
+    private static final String IS_USER_LOGGED_IN = "IS_USER_LOGGED_IN";
+    private static final String USER = "LOGGED_IN_USER";
+    private static final String AUTH_TOKEN = "AUTH_TOKEN";
+
+    public UserSession(Context context) {
+        sharedPreferences = context.getSharedPreferences("User Session", Context.MODE_PRIVATE);
+        editor = sharedPreferences.edit();
+    }
+
+    public void startSession(User user, String authToken) {
+
+        String userJson = new Gson().toJson(user);
+
+        editor.putBoolean(IS_USER_LOGGED_IN, true);
+        editor.putString(USER, userJson);
+        editor.putString(AUTH_TOKEN, authToken);
+
+        editor.commit();
+    }
+
+    public boolean getSession() {
+        return sharedPreferences.getBoolean(IS_USER_LOGGED_IN, false);
+    }
+
+    public User getUser() {
+
+        String userJson = sharedPreferences.getString(USER, "");
+        User user = new Gson().fromJson(userJson, User.class);
+
+        return user;
+    }
+
+    public String getAuthToken() {
+        return sharedPreferences.getString(AUTH_TOKEN, "");
+    }
+
+    public void endSession() {
+
+        editor.putBoolean(IS_USER_LOGGED_IN, false);
+        editor.remove(USER);
+        editor.remove(AUTH_TOKEN);
+        editor.commit();
+    }
 }
+
+
+
