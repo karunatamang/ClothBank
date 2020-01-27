225b0cbab2b02d3e7b886a08898a81fa92fc7bae
karunatamang <korunatamang12@gmail.com>
2020-01-26 16:53:15 -0800

    login response

--- app/src/main/java/com/example/clothbank/response/LoginResponse.java
+++ app/src/main/java/com/example/clothbank/response/LoginResponse.java
@@ -1,4 +1,17 @@
 package com.example.clothbank.response;
 
+import com.example.clothbank.model.User;
+
 public class LoginResponse {
+
+    private String token;
+    private User user;
+
+    public String getToken() {
+        return token;
+    }
+
+    public User getUser() {
+        return user;
+    }
 }
