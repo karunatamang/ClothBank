3ff8caa45c7687eb83c90fb4bcffae3fa75a29c6
karunatamang <korunatamang12@gmail.com>
2020-01-28 17:56:48 -0800

    change in donation model

--- app/src/main/java/com/example/clothbank/model/Donations.java
+++ app/src/main/java/com/example/clothbank/model/Donations.java
@@ -1,4 +1,22 @@
 package com.example.clothbank.model;
 
 public class Donations {
+    private String pickuptime, noofcloth, image;
+    private User donor;
+
+    public String getPickuptime() {
+        return pickuptime;
+    }
+
+    public String getNoofcloth() {
+        return noofcloth;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public User getDonor() {
+        return donor;
+    }
 }
