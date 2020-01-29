cbfe753bf408365a3dd9e4a5c4ca56e83210430d
karunatamang <korunatamang12@gmail.com>
2020-01-28 18:04:59 -0800

    donation Bll

--- app/src/main/java/com/example/clothbank/bll/DonationBll.java
+++ app/src/main/java/com/example/clothbank/bll/DonationBll.java
@@ -1,4 +1,56 @@
 package com.example.clothbank.bll;
 
+import com.example.clothbank.api.DonationApi;
+import com.example.clothbank.model.Donation;
+import com.example.clothbank.url.Url;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
 public class DonationBll {
+    private DonationApi donationApi;
+
+    public DonationBll() {
+        donationApi = Url.getInstance().create(DonationApi.class);
+    }
+
+    public Donation donate(Donation donation) {
+        Donation donations = null;
+        Call<Donation> donationCall = donationApi.donateCloth(donation);
+        try {
+            Response<Donation> donationResponse = donationCall.execute();
+            if (!donationResponse.isSuccessful()) {
+                return donations;
+            }
+            if (donationResponse.body() != null) {
+                donations = donationResponse.body();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return donations;
+    }
+
+    public boolean updateDonation(String id,Donation donation) {
+        boolean isDonationUpdated = false;
+
+        Call<String> donationCall = donationApi.updateDonation(id,donation);
+        try {
+            Response<String> donationResponse = donationCall.execute();
+            if (!donationResponse.isSuccessful()) {
+                return isDonationUpdated;
+            }
+            if (donationResponse.body() != null) {
+                isDonationUpdated = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return isDonationUpdated;
+    }
+
 }
