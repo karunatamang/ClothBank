f12cd15f7876f869491d5d97208095456882f3c8
karunatamang <korunatamang12@gmail.com>
2020-01-28 18:03:03 -0800

    add user Bll

--- app/src/main/java/com/example/clothbank/bll/UserBll.java
+++ app/src/main/java/com/example/clothbank/bll/UserBll.java
@@ -1,8 +1,10 @@
 package com.example.clothbank.bll;
 
+import com.example.clothbank.api.DonationApi;
 import com.example.clothbank.api.UserApi;
 import com.example.clothbank.model.Donation;
 import com.example.clothbank.model.User;
+import com.example.clothbank.response.ClothResponse;
 import com.example.clothbank.url.Url;
 
 import java.io.IOException;
@@ -14,9 +16,11 @@ public class UserBll {
 
     private UserApi userApi;
 
+
     public UserBll() {
         userApi = Url.getInstance().create(UserApi.class);
     }
+
 
     public boolean updateUser(String id, User user) {
         boolean isUserUpdated = false;
@@ -34,6 +38,42 @@ public class UserBll {
             e.printStackTrace();
         }
 
+
         return isUserUpdated;
+    }
+
+    public User userdetail(String id) {
+        User userdetail = null;
+
+        Call<User> clothcall = userApi.updateUser(id);
+        try {
+            Response<User> userResponse = clothcall.execute();
+            if (!userResponse.isSuccessful()) {
+                return userdetail;
+            }
+            if (userResponse.body() != null) {
+                userdetail = userResponse.body();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return userdetail;
+    }
+    public ClothResponse getNoofCloth(String donor) {
+        ClothResponse cloth = null;
+
+        Call<ClothResponse> userCall = userApi.getNoofCloth(donor);
+        try {
+            Response<ClothResponse> clothResponse = userCall.execute();
+            if (!clothResponse.isSuccessful()) {
+                return cloth;
+            }
+            if (clothResponse.body() != null) {
+                cloth = clothResponse.body();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return cloth;
     }
 }
