be0b6ef7668140eb79782ac6e73b00c69b0d970b
karunatamang <korunatamang12@gmail.com>
2020-01-30 23:09:22 -0800

    donation response

--- app/src/main/java/com/example/clothbank/response/DonationResponse.java
+++ app/src/main/java/com/example/clothbank/response/DonationResponse.java
@@ -1,4 +1,13 @@
 package com.example.clothbank.response;
 
+import com.example.clothbank.model.Donation;
+
+import java.util.List;
+
 public class DonationResponse {
+    private List<Donation> donation;
+
+    public List<Donation> getDonation() {
+        return donation;
+    }
 }
