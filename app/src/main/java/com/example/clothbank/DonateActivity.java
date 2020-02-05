4cf89c2b12f2acab1b483a0a11edf1f7f787c091
karunatamang <korunatamang12@gmail.com>
2020-02-04 20:41:38 -0800

    validation done

--- app/src/main/java/com/example/clothbank/DonateActivity.java
+++ app/src/main/java/com/example/clothbank/DonateActivity.java
@@ -104,34 +104,34 @@ public class DonateActivity extends AppCompatActivity {
         etName.setText(userSession.getUser().getFullname());
         etPhone.setText(userSession.getUser().getPhonenumber());
 
-//
-//        photo.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-//                startActivityForResult(intent, 1);
-//
-//            }
-//        });
-//
-//
-//        donate.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                donate();
-//                Intent intent = new Intent(DonateActivity.this, MainActivity.class);
-//                startActivity(intent);
-//                Notifications.givenotification(DonateActivity.this, "Donate Succesfully");
-//                Vibrator V =(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-//                long[] mVibratePattern = new long[]{0, 400, 200, 400};
-//                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
-//                    V.vibrate(mVibratePattern, -1);
-//                }else{
-//                    V.vibrate(mVibratePattern,-1);
-//                }
-//                finish();
-//            }
-//        });
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+
+            }
+        });
+
+
+        donate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                donate();
+                Intent intent = new Intent(DonateActivity.this, MainActivity.class);
+                startActivity(intent);
+                Notifications.givenotification(DonateActivity.this, "Donate Succesfully");
+                Vibrator V =(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                long[] mVibratePattern = new long[]{0, 400, 200, 400};
+                if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+                    V.vibrate(mVibratePattern, -1);
+                }else{
+                    V.vibrate(mVibratePattern,-1);
+                }
+                finish();
+            }
+        });
     }
 
 
